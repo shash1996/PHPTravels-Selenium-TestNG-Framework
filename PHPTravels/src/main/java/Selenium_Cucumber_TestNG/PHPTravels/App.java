41319578fa3e4505d762f5e5c9de25ae87dec283
shashwat <sha97katiyar@gmail.com>
2020-12-18 22:10:59 +0530

    First Commit

---
+++ PHPTravels/src/main/java/Selenium_Cucumber_TestNG/PHPTravels/App.java
@@ -0,0 +1,13 @@
+package Selenium_Cucumber_TestNG.PHPTravels;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ PHPTravels/src/test/java/Selenium_Cucumber_TestNG/PHPTravels/AppTest.java
@@ -0,0 +1,20 @@
+package Selenium_Cucumber_TestNG.PHPTravels;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
